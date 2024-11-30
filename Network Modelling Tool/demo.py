1c3d3268497200ea39b2f0be08b840ab0ee6b5d2
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-11-30 10:05:10 +0530

    updated demo

--- Network Modelling Tool/demo.py
+++ Network Modelling Tool/demo.py
@@ -2,8 +2,18 @@ import ipaddress
 
 class IPAddress:
     def __init__(self, ip):
-        # Using ipaddress module to handle IP validation
         self.ip = ipaddress.ip_address(ip)
         
     def __str__(self):
         return str(self.ip)
+
+class Device:
+    def __init__(self, name):
+        self.name = name
+        self.interfaces = {}  # Mapping interface name to IP addresses
+        
+    def add_interface(self, interface_name, ip):
+        self.interfaces[interface_name] = IPAddress(ip)
+    
+    def __str__(self):
+        return f"Device: {self.name}, Interfaces: {self.interfaces}"
